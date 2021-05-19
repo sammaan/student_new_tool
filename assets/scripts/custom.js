703e333978627161bb2b3b596c4bce59d03aa22b
aaashwani <ashwani.patel@talentedge.com>
2021-05-19 17:56:40 +0530

    tab as components, tab added in all pages, tob boxes added on all pages

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -7,3 +7,20 @@ $(document).ready(function(){
     var activePage = $('header').siblings('article').prop('id');
     $('.menu-bar').find('.link-'+ activePage).addClass('active');
 });
+
+//tablist
+
+function openSubject(evt, semName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("subject-selection-box");
+    tablinks = document.getElementsByClassName("tab-list");
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(semName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
